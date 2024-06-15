3922b6b52c6e3f925f8152773aa8eab58d495d73
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2024-06-15 13:38:51 +0100

    Fix weird iteration bug

--- docs/index.js
+++ docs/index.js
@@ -1,20 +1,19 @@
 showmoreBtn = document.getElementById("showmore");
 if (showmoreBtn) {
-  collapsibleItems = document.getElementsByClassName("img-container collapsible collapsed");
+  collapsibleItems = document.getElementsByClassName("img-container collapsible");
   showmoreBtn.textContent = "SHOW " + collapsibleItems.length + " MORE";
   showmoreBtn.addEventListener('click', function () {
     if (showmoreBtn.textContent.endsWith("MORE")) {
-      collapsibleItems = document.getElementsByClassName("img-container collapsible collapsed");
-      Array.prototype.forEach.call(collapsibleItems, item => {
-        item.classList.remove("collapsed");
-        showmoreBtn.textContent = "SHOW LESS";
-      });
+      collapsibleItems = document.getElementsByClassName("img-container collapsible");
+      for (item of collapsibleItems) {
+        item.classList.remove("collapsed");        showmoreBtn.textContent = "SHOW LESS";
+      }
     } else {
       collapsibleItems = document.getElementsByClassName("img-container collapsible");
-      Array.prototype.forEach.call(collapsibleItems, item => {
+      for (item of collapsibleItems) {
         item.classList.add("collapsed");
         showmoreBtn.textContent = "SHOW " + collapsibleItems.length + " MORE";
-      });
+      }
     }
   });
 }
